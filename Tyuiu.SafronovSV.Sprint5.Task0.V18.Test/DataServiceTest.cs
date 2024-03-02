e590db786271a70e13db522f756a80ea6c281f87
Safr0n0vSergei <144219000+Safr0n0vSergei@users.noreply.github.com>
2024-03-02 01:04:52 +0000

    Add files via upload

---
+++ Tyuiu.SafronovSV.Sprint5.Task0.V18.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.BorisovaDS.Sprint5.Task0.V18.Lib;
+
+namespace Tyuiu.BorisovaDS.Sprint5.Task0.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\Users\User\source\repos\Tyuiu.BorisovaDS.Sprint5\Tyuiu.BorisovaDS.Sprint5.Task0.V18\bin\Debug\OutPutFileTask0.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
