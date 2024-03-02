ce6cc33a00ec8a5a5bf0ec3761f5dc0289a91cf2
Safr0n0vSergei <144219000+Safr0n0vSergei@users.noreply.github.com>
2024-03-02 01:06:42 +0000

    Add files via upload

---
+++ Tyuiu.SafronovSV.Sprint5.Task4.V3.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.BorisovaDS.Sprint5.Task4.V3.Lib
+{
+    public class DataService : ISprint5Task4V3
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strX = File.ReadAllText(path);
+            double res = Math.Round(((Math.Sin(Convert.ToDouble(strX)) + 4) / (Convert.ToDouble(strX))) - 1.25 * Convert.ToDouble(strX), 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.SafronovSV.Sprint5.Task4.V3.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.BorisovaDS.Sprint5.Task4.V3.Lib;
+
+namespace Tyuiu.BorisovaDS.Sprint5.Task4.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidLoadFromDataFile()
+        {
+            string path = @"C:\Users\User\source\repos\Tyuiu.BorisovaDS.Sprint5\Tyuiu.BorisovaDS.Sprint5.Task4.V3\bin\Debug\InPutDataFileTask4V3.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            Assert.AreEqual(true, fileExists);
+        }
+    }
+}
